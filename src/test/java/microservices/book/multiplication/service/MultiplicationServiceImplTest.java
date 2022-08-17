ded4c99e23730fa810df1c98989b2021ecfcf65a
André R.B. (mac) <bronca.andre@gmail.com>
2022-08-17 13:02:37 -0300

    configurando os testes e alinhando o projeto

--- src/test/java/microservices/book/multiplication/service/MultiplicationServiceImplTest.java
+++ src/test/java/microservices/book/multiplication/service/MultiplicationServiceImplTest.java
@@ -9,6 +9,8 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
 import microservices.book.multiplication.domain.Multiplication;
+import microservices.book.multiplication.domain.MultiplicationResultAttempt;
+import microservices.book.multiplication.domain.User;
 
 public class MultiplicationServiceImplTest {
 
@@ -35,7 +37,34 @@ public class MultiplicationServiceImplTest {
         // assert
         assertThat(multiplication.getFactorA()).isEqualTo(50);
         assertThat(multiplication.getFactorB()).isEqualTo(30);
-        assertThat(multiplication.getResult()).isEqualTo(1500);
+    }
+    
+    @Test
+    public void checkCorrectAttemptTest() {
+    	//given
+    	Multiplication multiplication = new Multiplication(50, 60);
+    	User user = new User("john_doe");
+    	MultiplicationResultAttempt attempt = new MultiplicationResultAttempt(user, multiplication, 3000);
+    	
+    	//when
+    	boolean attemptResult = multiplicationServiceImpl.checkAttempt(attempt);
+    	
+    	//assert
+    	assertThat(attemptResult).isTrue();
+    }
+    
+    @Test
+    public void checkWrongAttemptTest() {
+    	//given
+    	Multiplication multiplication = new Multiplication(50, 60);
+    	User user = new User("John_doe");
+    	MultiplicationResultAttempt attempt = new MultiplicationResultAttempt(user, multiplication, 3010);
+    	
+    	//when
+    	boolean attemptResult = multiplicationServiceImpl.checkAttempt(attempt);
+    	
+    	//assert
+    	assertThat(attemptResult).isFalse();
     }
 
 }
